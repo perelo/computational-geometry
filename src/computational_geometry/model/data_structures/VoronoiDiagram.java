45493634ecf80bae8283d77ec835c63429222f45
Eloi Perdereau <eloi@perdereau.eu>
2013-05-19 00:31:09 +0200

    add computation of Delaunay's triangulation as dual

--- src/computational_geometry/model/data_structures/VoronoiDiagram.java
+++ src/computational_geometry/model/data_structures/VoronoiDiagram.java
@@ -1,6 +1,11 @@
 package computational_geometry.model.data_structures;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import computational_geometry.model.beans.Point;
+import computational_geometry.model.beans.Segment;
 
 /**
  * Voronoi diagram
@@ -37,6 +42,26 @@ public class VoronoiDiagram extends DCEL {
                     "Can only add VorCells to VoronoiDiagram.");
         }
         super.addFace(f);
+    }
+
+    /**
+     * Compute the dual of this VoronoiDiagram
+     * @return the segments belonging to the Delaunay's triangulation
+     */
+    public List<Segment> computeDelaunay() {
+        List<Segment> del = new ArrayList<Segment>();
+        for (Iterator<Edge> it = getEdgeIterator(); it.hasNext(); ) {
+            Edge e = it.next();
+            if (!e.isDrawn) {
+                del.add(new Segment(((VorCell)e.getFace()).site, ((VorCell)e.getTwin().getFace()).site));
+                e.isDrawn = true;
+                e.getTwin().isDrawn = true;
+            }
+        }
+        for (Iterator<Edge> it = getEdgeIterator(); it.hasNext(); ) {
+            it.next().isDrawn = false;
+        }
+        return del;
     }
 
 }
